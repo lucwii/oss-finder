edce124d5937cdd11f44263dbacbd78096f620cb
lucwii <milanoviclukaa23@gmail.com>
2026-04-27 16:05:46 +0200

    feat: env reading added to the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubModule } from './github/github.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GithubModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GithubModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
