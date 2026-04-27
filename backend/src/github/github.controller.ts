b44c3ee881d653f4ea12ae2487676d0577f8ad7c
lucwii <milanoviclukaa23@gmail.com>
2026-04-27 16:03:21 +0200

    feat: github recommendations api made

--- backend/src/github/github.controller.ts
+++ backend/src/github/github.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { GithubService } from './github.service';
 
 @Controller('github')
-export class GithubController {}
+export class GithubController {
+    constructor(private readonly githubService: GithubService) {}
+
+    @Get('recommendations')
+    async getRecommendations(
+        @Query('language') language: string,
+        @Query('level') level: string
+    ) {
+        return this.githubService.getRecommendations(language, level);
+    }
+}
