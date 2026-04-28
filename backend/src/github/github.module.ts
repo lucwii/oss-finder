4b0de4846b47c8b33b0c2d812999fd5a2c5eda49
lucwii <milanoviclukaa23@gmail.com>
2026-04-28 13:02:15 +0200

    fix: github module fix

--- backend/src/github/github.module.ts
+++ backend/src/github/github.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { GithubService } from './github.service';
 import { GithubController } from './github.controller';
 
 @Module({
+  imports: [HttpModule],
   providers: [GithubService],
   controllers: [GithubController]
 })
