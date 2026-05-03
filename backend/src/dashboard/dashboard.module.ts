7549035668925bd5d4bf591def559a582da9c99f
lucwii <milanoviclukaa23@gmail.com>
2026-05-03 19:39:10 +0200

    feat (dashboard): dashboard module added and imported

--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { GithubModule } from 'src/github/github.module';
+import { OnboardingModule } from 'src/onboarding/onboarding.module';
 
 @Module({
+  imports: [
+    GithubModule,
+    OnboardingModule,
+  ],
   controllers: [DashboardController],
-  providers: [DashboardService]
+  providers: [DashboardService],
 })
 export class DashboardModule {}
