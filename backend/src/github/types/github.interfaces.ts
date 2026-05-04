fda5a85d8779cda57bbabfda2181024060fbb4c5
lucwii <milanoviclukaa23@gmail.com>
2026-05-04 16:11:38 +0200

    feat (github): interfaces changes

--- backend/src/github/types/github.interfaces.ts
+++ backend/src/github/types/github.interfaces.ts
@@ -1,15 +1,3 @@
-export interface GithubRepo {
-  id: number;
-  name: string;
-  full_name: string;
-  description: string;
-  html_url: string;
-  stargazers_count: number;
-  open_issues_count: number;
-  updated_at: string;   
-  language: string;
-}
-
 export interface GithubIssue {
   id: number;
   title: string;
@@ -22,4 +10,28 @@ export interface RepoRecommendation {
   repo: GithubRepo;
   score: number;
   issues: GithubIssue[];  
+}
+
+export interface ScoredRepo {
+  repo: GithubRepo;
+  score: number;
+  match_percentage: number;
+  issues: GithubIssue[];
+}
+
+
+export interface GithubRepo {
+  id: number;
+  name: string;
+  full_name: string;
+  description: string;
+  html_url: string;
+  stargazers_count: number;
+  open_issues_count: number;
+  updated_at: string;
+  language: string;
+  size: number;      
+  topics: string[];  
+  has_wiki: boolean; 
+  license: { name: string } | null;
 }
